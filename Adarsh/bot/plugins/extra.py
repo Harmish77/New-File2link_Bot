80e186955dc230cc19a11f5cac85b1c19edfb009
Harmish77 <107766922+Harmish77@users.noreply.github.com>
2023-07-27 19:27:24 +0530

    Update extra.py

--- Adarsh/bot/plugins/extra.py
+++ Adarsh/bot/plugins/extra.py
@@ -17,12 +17,12 @@ async def maintainers(b,m):
     except Exception:
                 await b.send_message(
                     chat_id=m.chat.id,
-                    text="I am Coded By [AMIR BISTA](https://t.me/Ab_Admin0)",
+                    text="I am Coded By [Harmish](https://t.me/Mr_HKs)",
                     
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("Developer💻", url=f"https://t.me/Ab_Admin0")
+                                InlineKeyboardButton("Developer💻", url=f"https://t.me/Mr_HKs")
                             ]
                         ]
                     ),
@@ -42,7 +42,7 @@ async def follow_user(b,m):
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("Subscribe Me", url=f"https://t.me/AB_BotZ_Update")
+                                InlineKeyboardButton("Subscribe Me", url=f"https://t.me/Mr_HKs")
                             ]
                         ]
                     ),
